2d8e9528278edde156a92c71475df39dc8543aa4
Ethan Jewett <esjewett@gmail.com>
2021-01-13 10:37:31 -0600

    Move to synchronous processes

--- index.js
+++ index.js
@@ -2,90 +2,40 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const artifact = require('@actions/artifact');
 const kill  = require('tree-kill');
-const { exec, spawn } = require("child_process");
+const { exec, spawn, execSync } = require("child_process");
 
 try {
   const configFile = core.getInput('config-file-location') ? core.getInput('config-file-location') : "ci.yaml";
-  const projectPath = core.getInput('project-path') ? core.getInput('project-path') : "node_modules/msp_transPost";
+  const projectPath = process.env.GITHUB_WORKSPACE;
   console.log(`Config file: ${configFile}`);
 
-  const build = exec(`cd ${projectPath} && npm install @sap/ui5-builder-webide-extension@1.0.11 && ui5 build dev --all --config=${configFile}`);
+  const build = execSync(`cd ${projectPath} && npm install @sap/ui5-builder-webide-extension@1.0.11 && ui5 build dev --all --config=${configFile}`);
 
-  build.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+  console.log(`stdout: ${build}`);
+
+  const server = spawn(`cd ${projectPath} && ui5 serve`, {
+    shell: "/bin/bash"
   });
 
-  build.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  })
-  
-  build.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-  });
-  
-  build.on('exit', (code) => {
-    console.log(`child process exited with code ${code}`);
+  const axeRunner = execSync("axe http://localhost:8080/index.html --exit --load-delay=3000 | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' > axe.log");
 
-    const server = spawn(`cd ${projectPath} && ui5 serve`, {
-      shell: "/bin/bash"
-    });
+  core.setOutput("axe-results-location", "axe.log");
+  kill(server.pid);
 
-    server.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+  // if(process.env.ACTIONS_RUNTIME_TOKEN) {
+  //   const artifactClient = artifact.create()
+  //   const artifactName = 'axe-output';
+  //   const files = [
+  //       'axe-output.txt'
+  //   ]
 
-      if(data === "URL: http://localhost:8080") {
-        console.log("Server started");
-      }
-    });
+  //   const rootDirectory = '.' // Also possible to use __dirname
+  //   const options = {
+  //       continueOnError: false
+  //   }
 
-    server.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    })
-    
-    server.on('close', (code) => {
-      console.log(`child process close all stdio with code ${code}`);
-    });
-    
-    server.on('exit', (code) => {
-      console.log(`child process exited with code ${code}`);
-
-    });
-
-    const axeRunner = exec("axe http://localhost:8080/index.html --exit --load-delay=3000 | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' > axe.log");
-
-    axeRunner.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    })
-
-    axeRunner.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-    })
-    
-    axeRunner.on('close', (code) => {
-      console.log(`child process close all stdio with code ${code}`);
-    });
-    
-    axeRunner.on('exit', async (code) => {
-      console.log(`child process exited with code ${code}`);
-      core.setOutput("axe-results-location", "axe.log");
-      kill(server.pid);
-
-      // if(process.env.ACTIONS_RUNTIME_TOKEN) {
-      //   const artifactClient = artifact.create()
-      //   const artifactName = 'axe-output';
-      //   const files = [
-      //       'axe-output.txt'
-      //   ]
-
-      //   const rootDirectory = '.' // Also possible to use __dirname
-      //   const options = {
-      //       continueOnError: false
-      //   }
-
-      //   const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
-      // }
-    });
-  });
+  //   const uploadResponse = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+  // }
 } catch (error) {
   core.setFailed(error.message);
 }
