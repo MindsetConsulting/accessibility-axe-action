15b381739ee4ed0c3da081f0b57757b60533eea8
Ethan Jewett <esjewett@gmail.com>
2021-01-19 13:40:18 -0600

    Massive simplification

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const artifact = require('@actions/artifact');
-const kill  = require('tree-kill');
-const { exec, spawn, execSync } = require("child_process");
+const { execSync } = require("child_process");
 
 try {
-  const configFile = core.getInput('build-config-file') ? core.getInput('build-config-file') : "ui5.yaml";
-  const configFileServe = core.getInput('serve-config-file') ? core.getInput('serve-config-file') : "ui5.yaml";
-  const projectPath = process.env.GITHUB_WORKSPACE;
-  core.info(`Build config file: ${configFile}`);
-  core.info(`Serve config file: ${configFileServe}`);
+  const location = core.getInput('location') ? core.getInput('location') : "http://localhost:8080/index.html";
+  core.info(`Running on: ${location}`);
 
-  const build = execSync(`cd ${projectPath} && npm install @sap/ui5-builder-webide-extension@1.0.11 && npx ui5 build dev --all --config=${configFile}`);
-
-  core.info(build);
-
-  const server = spawn(`cd ${projectPath} && npx ui5 serve --config=${configFile}`, {
-    shell: "/bin/bash"
-  });
-
-  core.info("Spawned server");
-
-  const axeRunner = execSync("npx axe http://localhost:8080/index.html --load-delay=3000");
+  const axeRunner = execSync(`npx axe ${location} --load-delay=3000`);
 
   core.info(axeRunner);
-
-  kill(server.pid);
 } catch (error) {
   core.setFailed(error.message);
 }
