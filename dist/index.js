440e9d9739357cd87adc5f19bf2a3defb2fdb62f
Ethan Jewett <esjewett@gmail.com>
2021-01-19 15:14:27 -0600

    Initial error reporting

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ module.exports =
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(186);
+const fs = __nccwpck_require__(747);
 const { execSync } = __nccwpck_require__(129);
 
 try {
@@ -13,11 +14,30 @@ try {
   const loadDelay = core.getInput('load-delay') ? core.getInput('load-delay') : '5000';
   core.info(`Running on: ${location}`);
 
-  const axeRunner = execSync(`npx axe ${location} --load-delay=${loadDelay}`);
+  const axeRunner = execSync(`npx axe ${location} --load-delay=${loadDelay} --save=axe.json.log --exit`);
 
   core.info(axeRunner);
 } catch (error) {
-  core.setFailed(error.message);
+  if(error.status === 1) {
+    core.info("Exit code 1");
+    try {
+      const data = JSON.parse(fs.readFileSync('axe.json.log', 'utf8'));
+      data[0].violations.forEach(violation => {
+        core.error(violation.help + "\n  " + violation.helpUrl);
+        core.startGroup("Nodes");
+        violation.nodes.forEach(node => {
+          core.info(node.failureSummary);
+          core.info(node.html);
+        })
+        core.endGroup();
+      })
+    } catch (error) {
+      core.setFailed("Failed to read log file axe.json.log.");
+    }
+  } else {
+    core.info("Exit code <> 1");
+    core.setFailed(error.stdout);
+  }
 }
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const fs = require('fs');
 const { execSync } = require("child_process");
 
 try {
@@ -6,9 +7,28 @@ try {
   const loadDelay = core.getInput('load-delay') ? core.getInput('load-delay') : '0';
   core.info(`Running on: ${location}`);
 
-  const axeRunner = execSync(`npx axe ${location} --load-delay=${loadDelay}`);
+  const axeRunner = execSync(`npx axe ${location} --load-delay=${loadDelay} --save=axe.json.log --exit`);
 
   core.info(axeRunner);
 } catch (error) {
-  core.setFailed(error.message);
+  if(error.status === 1) {
+    core.info("Exit code 1");
+    try {
+      const data = JSON.parse(fs.readFileSync('axe.json.log', 'utf8'));
+      data[0].violations.forEach(violation => {
+        core.error(violation.help + "\n  " + violation.helpUrl);
+        core.startGroup("Nodes");
+        violation.nodes.forEach(node => {
+          core.info(node.failureSummary);
+          core.info(node.html);
+        })
+        core.endGroup();
+      })
+    } catch (error) {
+      core.setFailed("Failed to read log file axe.json.log.");
+    }
+  } else {
+    core.info("Exit code <> 1");
+    core.setFailed(error.stdout);
+  }
 }
